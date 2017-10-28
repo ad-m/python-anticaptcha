de53537e27790969d475c5f3fe0982cd6c19cc4c
Adam Dobrawy <ad-m@users.noreply.github.com>
2017-10-28 12:49:24 +0200

    Add basic proxy support & docs proxy server setup  (#8)

--- examples/funcaptcha.py
+++ examples/funcaptcha.py
@@ -1,6 +1,8 @@
 import requests
 from os import environ
 import re
+from random import choice
+
 from python_anticaptcha import AnticaptchaClient, FunCaptchaTask, Proxy
 
 api_key = environ['KEY']
@@ -12,7 +14,8 @@ session = requests.Session()
 UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 ' \
      '(KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
 session.headers = {'User-Agent': UA}
-proxy = Proxy.parse_url("socks5://user:password@123.123.123.123:1234/")
+proxy_url = choice(environ['PROXY_URL'].split(','))
+proxy = Proxy.parse_url(proxy_url)
 
 
 def get_form_html():
@@ -23,8 +26,8 @@ def get_token(form_html):
     site_key = re.search(site_key_pattern, form_html).group(1)
     task = FunCaptchaTask(url, site_key, proxy=proxy, user_agent=UA)
     job = client.createTask(task)
-    job.join()
-    return job.get_solution_response()
+    job.join(maximum_time=10**4)
+    return job.get_token_response()
 
 
 if __name__ == '__main__':
