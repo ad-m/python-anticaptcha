f2e2610702d6a38a909c94cd3df606a38d3d923a
Adam Dobrawy <naczelnik@jawnosc.tk>
2018-02-26 11:14:12 +0100

    Bump version: 0.1.1 → 0.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-version = '0.1.1'
+version = '0.2.0'
 
 if sys.argv[-1] == 'publish':
     try:
