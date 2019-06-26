9f069cf4fe634f34ccda29c18c03c63db04fe199
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-06-26 02:00:56 +0200

    Update Funcaptcha example

--- examples/funcaptcha.py
+++ examples/funcaptcha.py
@@ -1,9 +1,11 @@
+from urllib.parse import urlparse
+
 import requests
 from os import environ
 import re
 from random import choice
 
-from python_anticaptcha import AnticaptchaClient, FunCaptchaTask, Proxy
+from python_anticaptcha import AnticaptchaClient, FunCaptchaTask
 
 api_key = environ['KEY']
 site_key_pattern = 'data-pkey="(.+?)"'
@@ -17,13 +19,24 @@ session.headers = {'User-Agent': UA}
 proxy_urls = environ['PROXY_URL'].split(',')
 
 
+def parse_url(url):
+    parsed = urlparse(url)
+    return dict(
+        proxy_type=parsed.scheme,
+        proxy_address=parsed.hostname,
+        proxy_port=parsed.port,
+        proxy_login=parsed.username,
+        proxy_password=parsed.password
+    )
+
+
 def get_form_html():
     return session.get(url).text
 
 
 def get_token(form_html):
     proxy_url = choice(proxy_urls)
-    proxy = Proxy.parse_url(proxy_url)
+    proxy = parse_url(proxy_url)
 
     site_key = re.search(site_key_pattern, form_html).group(1)
     task = FunCaptchaTask(url, site_key, proxy=proxy, user_agent=UA)
