857b03b344d41121de225a2bd246d9f0050e92ae
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-06-28 23:48:20 +0200

    Bump version: 0.3.2 → 0.4.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-version = '0.3.2'
+version = '0.4.0'
 
 tests_deps = [
     'nose',
