581a1aebbc40867374803aebd40976ddccc0ec05
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-06-21 14:18:19 +0200

    Add example for balance

---
+++ examples/balance.py
@@ -0,0 +1,13 @@
+from os import environ
+from pprint import pprint
+from python_anticaptcha import AnticaptchaClient, ImageToTextTask
+from sys import argv
+api_key = environ["KEY"]
+
+
+def process():
+    client = AnticaptchaClient(api_key)
+    pprint(client.getBalance())
+
+if __name__ == "__main__":
+    pprint(process())
